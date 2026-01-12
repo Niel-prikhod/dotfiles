39e7012d5e2757ee3548bb891a26fbde86487db8
Niel <dyprih@gmail.com>
2026-01-12 12:46:47 +0100

    remap haproon

--- nvim/lua/niel/plugins/harpoon.lua
+++ nvim/lua/niel/plugins/harpoon.lua
@@ -1,5 +1,5 @@
 return {
-	'ThePrimeagen/harpoon', 
+	'ThePrimeagen/harpoon',
 
 	dependencies = {
 		"nvim-lua/plenary.nvim"
@@ -12,10 +12,11 @@ return {
 		vim.keymap.set("n", "<leader>a", mark.add_file)
 		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-		vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
-		vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
-		vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
-		vim.keymap.set("n", "<C-;>", function() ui.nav_file(4) end)
+		vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+		vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+		vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+		vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+		vim.keymap.set("n", "<C-n>", function() ui.nav_file(5) end)
+		vim.keymap.set("n", "<C-m>", function() ui.nav_file(6) end)
 	end
 }
-
